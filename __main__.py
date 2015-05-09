b7039950db8fc30026ce4da9099a045a467ad320
Juanjo Alvarez <juanjo@juanjoalvarez.net>
2015-05-09 17:45:33 +0200

    Added W, E and B motions (same as in Vi)

--- __main__.py
+++ __main__.py
@@ -29,8 +29,7 @@ import sys, os, enum
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui     import *
 from PyQt5.QtCore    import Qt
-from PyQt5.Qsci      import QsciScintilla, QsciLexerPython
-from copy import copy
+from PyQt5.Qsci      import QsciScintilla as QSci, QsciLexerPython
 
 # FIXME: make these configurable
 ESCAPEFIRST     = Qt.Key_K
@@ -40,6 +39,7 @@ RETURN_LINES    = 5
 NUMSETKEYS = {Qt.Key_0: '0', Qt.Key_1: '1', Qt.Key_2: '2', Qt.Key_3: '3',
               Qt.Key_4: '4', Qt.Key_5: '5', Qt.Key_6: '6', Qt.Key_7: '7',
               Qt.Key_8: '8', Qt.Key_9: '9'}
+WHITESPACE = {32, 10, 13, 9} 
 
 
 class EditorMode(enum.Enum):
@@ -48,7 +48,7 @@ class EditorMode(enum.Enum):
     Command  = 3
 
 
-class NemeTextWidget(QsciScintilla):
+class NemeTextWidget(QSci):
     ARROW_MARKER_NUM = 0
 
     def __init__(self, parent=None):
@@ -72,13 +72,13 @@ class NemeTextWidget(QsciScintilla):
         self.setMarginsBackgroundColor(QColor("#cccccc"))
         self.setMarginSensitivity(1, True)
         self.marginClicked.connect(self.on_margin_clicked)
-        self.markerDefine(QsciScintilla.RightArrow, self.ARROW_MARKER_NUM)
+        self.markerDefine(QSci.RightArrow, self.ARROW_MARKER_NUM)
         self.setMarkerBackgroundColor(QColor("#ee1111"), self.ARROW_MARKER_NUM)
 
         # Brace matching: enable for a brace immediately before or after
         # the current position
         #
-        self.setBraceMatching(QsciScintilla.SloppyBraceMatch)
+        self.setBraceMatching(QSci.SloppyBraceMatch)
 
         # Current line visible with special background color
         self.setCaretLineVisible(True)
@@ -87,7 +87,7 @@ class NemeTextWidget(QsciScintilla):
         # Don't want to see the horizontal scrollbar at all
         # Use raw message to Scintilla here (all messages are documented
         # here: http://www.scintilla.org/ScintillaDoc.html)
-        self.SendScintilla(QsciScintilla.SCI_SETHSCROLLBAR, 0)
+        self.SendScintilla(QSci.SCI_SETHSCROLLBAR, 0)
 
         # Set Python lexer
         # Set style for Python comments (style number 1) to a fixed-width
@@ -105,6 +105,46 @@ class NemeTextWidget(QsciScintilla):
         self.prevWasEscapeFirst = False # used for kj escape secuence
 
 
+    def _findWORDPosition(self, findRight=True):
+        """
+        Find next WORD. With findRight=True it will
+        instead find the end of the previous WORD
+        """
+        source = bytearray(1)
+        currentPos = self.SendScintilla(QSci.SCI_GETCURRENTPOS)
+        char = -1
+        foundWhiteSpace = False
+        adder = 1 if findRight else -1
+
+        while char != 0:
+            char = self.SendScintilla(QSci.SCI_GETCHARAT, currentPos)
+            if foundWhiteSpace and char not in WHITESPACE:
+                # found the next word
+                return currentPos
+
+            if not foundWhiteSpace:
+                foundWhiteSpace = char in WHITESPACE
+            currentPos += adder 
+        return -1
+
+
+    def _findWORDExtremePosition(self, start = True):
+        """
+        Find the start or end of the current WORD
+        """
+        source = bytearray(1)
+        currentPos = self.SendScintilla(QSci.SCI_GETCURRENTPOS)
+        char = -1
+        adder = -1 if start else 1
+
+        while char != 0:
+            char = self.SendScintilla(QSci.SCI_GETCHARAT, currentPos)
+            if char in WHITESPACE:
+                return currentPos - adder
+            currentPos += adder
+        return -1
+
+
     def _processNumberPrefix(self, key):
         "This should be called only when key has been validated to be a member of NUMSETKEYS"
 
@@ -113,8 +153,8 @@ class NemeTextWidget(QsciScintilla):
 
         if strnumkey == '0' and not len(self.numberList):
             # store 0s only when not the first
-           self.numberList.clear()
-           haveToClearList = True
+            self.numberList.clear()
+            haveToClearList = True
         else:
             self.numberList.append(strnumkey)
 
@@ -161,7 +201,7 @@ class NemeTextWidget(QsciScintilla):
                     if self.prevWasEscapeFirst:
                         # delete previous K and change to Movement
                         # FIXME: delete previous k
-                        self.SendScintilla(QsciScintilla.SCI_DELETEBACK)
+                        self.SendScintilla(QSci.SCI_DELETEBACK)
                         self.setMode(EditorMode.Movement)
                     else:
                         process = True
@@ -176,79 +216,82 @@ class NemeTextWidget(QsciScintilla):
             elif modifiers == Qt.ControlModifier: # CONTROL
                 # Ctrl + IK is PageUP/Down too like in normal mode
                 if e.key() == Qt.Key_I:
-                    self.SendScintilla(QsciScintilla.SCI_PAGEUP)
+                    self.SendScintilla(QSci.SCI_PAGEUP)
                 elif e.key() == Qt.Key_K:
-                    self.SendScintilla(QsciScintilla.SCI_PAGEDOWN)
+                    self.SendScintilla(QSci.SCI_PAGEDOWN)
 
             elif modifiers == Qt.AltModifier: # ALT
                 # Alt + IKJL also moves the cursor in typing mode
                 if e.key() == Qt.Key_I:
-                    self.SendScintilla(QsciScintilla.SCI_LINEUP)
+                    self.SendScintilla(QSci.SCI_LINEUP)
                 elif e.key() == Qt.Key_K:
-                    self.SendScintilla(QsciScintilla.SCI_LINEDOWN)
+                    self.SendScintilla(QSci.SCI_LINEDOWN)
                 elif e.key() == Qt.Key_J:
-                    self.SendScintilla(QsciScintilla.SCI_CHARLEFT)
+                    self.SendScintilla(QSci.SCI_CHARLEFT)
                 elif e.key() == Qt.Key_L:
-                    self.SendScintilla(QsciScintilla.SCI_CHARRIGHT)
+                    self.SendScintilla(QSci.SCI_CHARRIGHT)
 
         # =============================================================
         # Movement Mode
         # =============================================================
 
         elif self.mode == EditorMode.Movement:
-            # If there is a number, add it to self.numberList. If not, clear it.
 
             if modifiers == Qt.NoModifier: # NO MODIFIER
                 if e.key() in NUMSETKEYS:
                     clearnumberList = self._processNumberPrefix(e.key())
+                    if e.key() == Qt.Key_0 and clearnumberList:
+                        # 0 with buffer empty = goto beginning of line
+                        self.SendScintilla(QSci.SCI_HOME)
+                        
                 elif e.key() in {Qt.Key_T, Qt.Key_A}:
-                    self.SendScintilla(QsciScintilla.SCI_CHARRIGHT)
+                    self.SendScintilla(QSci.SCI_CHARRIGHT)
                     self.setMode(EditorMode.Typing)
                 elif e.key() == Qt.Key_Space:
                     self.setMode(EditorMode.Command)
                 elif e.key() == Qt.Key_I: # line up
                     if modifiers == Qt.ControlModifier:
                         for _ in range(self.getNumberPrefix(True)):
-                            self.SendScintilla(QsciScintilla.SCI_PAGEUP)
+                            self.SendScintilla(QSci.SCI_PAGEUP)
                     else:
                         for _ in range(self.getNumberPrefix(True)):
-                            self.SendScintilla(QsciScintilla.SCI_LINEUP)
+                            self.SendScintilla(QSci.SCI_LINEUP)
                 elif e.key() == Qt.Key_Backspace: # n lines up
                     for _ in range(BACKSPACE_LINES * self.getNumberPrefix(True)):
-                        self.SendScintilla(QsciScintilla.SCI_LINEUP, 5)
+                        self.SendScintilla(QSci.SCI_LINEUP, 5)
                 elif e.key() == Qt.Key_K: # line down
                     if modifiers == Qt.ControlModifier:
                         for _ in range(self.getNumberPrefix(True)):
-                            self.SendScintilla(QsciScintilla.SCI_PAGEDOWN)
+                            self.SendScintilla(QSci.SCI_PAGEDOWN)
                     else:
                         for _ in range(self.getNumberPrefix(True)):
-                            self.SendScintilla(QsciScintilla.SCI_LINEDOWN)
+                            self.SendScintilla(QSci.SCI_LINEDOWN)
                 elif e.key() == Qt.Key_Return: # n lines up
                     # FIXME: MUST be a better way...
                     for _ in range(RETURN_LINES * self.getNumberPrefix(True)):
-                        self.SendScintilla(QsciScintilla.SCI_LINEDOWN, 5)
+                        self.SendScintilla(QSci.SCI_LINEDOWN, 5)
                 elif e.key() == Qt.Key_J: # char left
                     for _ in range(self.getNumberPrefix()):
-                        self.SendScintilla(QsciScintilla.SCI_CHARLEFT)
+                        self.SendScintilla(QSci.SCI_CHARLEFT)
                 elif e.key() == Qt.Key_L: # char right
                     for _ in range(self.getNumberPrefix()):
-                        self.SendScintilla(QsciScintilla.SCI_CHARRIGHT)
+                        self.SendScintilla(QSci.SCI_CHARRIGHT)
                 elif e.key() == Qt.Key_W: # next beginning of word
                     for _ in range(self.getNumberPrefix()):
-                        self.SendScintilla(QsciScintilla.SCI_WORDRIGHT)
+                        self.SendScintilla(QSci.SCI_WORDRIGHT)
                 elif e.key() == Qt.Key_B: # prev beginning of word
                     for _ in range(self.getNumberPrefix()):
-                        self.SendScintilla(QsciScintilla.SCI_WORDLEFT)
+                        self.SendScintilla(QSci.SCI_WORDLEFT)
                 elif e.key() == Qt.Key_E: # next end of word
                     for _ in range(self.getNumberPrefix()):
-                        self.SendScintilla(QsciScintilla.SCI_WORDRIGHTEND)
+                        self.SendScintilla(QSci.SCI_WORDRIGHTEND)
                 elif e.key() == Qt.Key_U: # undo
                     self.setReadOnly(0)
                     for _ in range(self.getNumberPrefix()):
                         self.undo()
                     self.setReadOnly(1)
                 elif e.key() == Qt.Key_S: # first non-blank in line
-                    self.SendScintilla(QsciScintilla.SCI_VCHOME)
+                    self.SendScintilla(QSci.SCI_VCHOME)
                 elif e.key() == Qt.Key_O: # insert empty line below current
                     self.beginUndoAction()
                     # FIXME: start at the right column after language indentation
@@ -264,12 +307,12 @@ class NemeTextWidget(QsciScintilla):
 
             elif modifiers == Qt.ShiftModifier: # SHIFT
                 if e.key() == Qt.Key_Dollar: # end of line
-                    self.SendScintilla(QsciScintilla.SCI_LINEEND)
+                    self.SendScintilla(QSci.SCI_LINEEND)
                 elif e.key() == Qt.Key_A: # append after EOL
-                    self.SendScintilla(QsciScintilla.SCI_LINEEND)
+                    self.SendScintilla(QSci.SCI_LINEEND)
                     self.setMode(EditorMode.Typing)
                 elif e.key() == Qt.Key_I: # insert at the start of the line
-                    self.SendScintilla(QsciScintilla.SCI_VCHOME)
+                    self.SendScintilla(QSci.SCI_VCHOME)
                     self.setMode(EditorMode.Typing)
                 elif e.key() == Qt.Key_O: # insert empty line above current
                     # FIXME: start at the right column after language indentation
@@ -279,33 +322,44 @@ class NemeTextWidget(QsciScintilla):
                         self.insertAt('\n', line, 0)
                         self.setCursorPosition(line, 0)
                         self.setMode(EditorMode.Typing)
-                        #line, index = self.getCursorPosition()
-                        #self.setCursorPosition(line-1, 0)
                     self.endUndoAction()
                 elif e.key() == Qt.Key_J: # join line with line below
                     # FIXME: undoing this leaves the cursor at the end of the line
                     self.beginUndoAction()
                     for _ in range(self.getNumberPrefix(True)):
                         line, index = self.getCursorPosition()
-                        curLine     = self.text(line).rstrip()
                         nextLine    = self.text(line + 1).lstrip()
                         if not nextLine:
                             nextLine = '\n'
 
                         self.setReadOnly(0)
                         self.insertAt(' ' + nextLine, line, self.lineLength(line)-1)
-                        self.SendScintilla(QsciScintilla.SCI_LINEDOWN)
-                        self.SendScintilla(QsciScintilla.SCI_LINEDELETE)
-                        self.SendScintilla(QsciScintilla.SCI_LINEDELETE)
-                        self.SendScintilla(QsciScintilla.SCI_LINEUP)
+                        self.SendScintilla(QSci.SCI_LINEDOWN)
+                        self.SendScintilla(QSci.SCI_LINEDELETE)
+                        self.SendScintilla(QSci.SCI_LINEDELETE)
+                        self.SendScintilla(QSci.SCI_LINEUP)
                         self.setReadOnly(1)
                     self.endUndoAction()
+                elif e.key() == Qt.Key_W: # next WORD
+                    nextWordPos = self._findWORDPosition()
+                    if nextWordPos != -1:
+                        self.SendScintilla(QSci.SCI_GOTOPOS, nextWordPos)
+                elif e.key() == Qt.Key_E: # prev WORD end
+                    prevWordEndPos = self._findWORDPosition(findRight=False)
+                    if prevWordEndPos != -1:
+                        self.SendScintilla(QSci.SCI_GOTOPOS, prevWordEndPos)
+                elif e.key() == Qt.Key_B: # prev WORD start
+                    prevWordEndPos = self._findWORDPosition(findRight=False)
+                    if prevWordEndPos != -1:
+                        self.SendScintilla(QSci.SCI_GOTOPOS, prevWordEndPos)
+                        wordStart = self._findWORDExtremePosition(start = True)
+                        self.SendScintilla(QSci.SCI_GOTOPOS, wordStart)
 
 
             elif modifiers == Qt.AltModifier: # ALT
                 if e.key() == Qt.Key_E: # prev end of word
                     for _ in range(self.getNumberPrefix()):
-                        self.SendScintilla(QsciScintilla.SCI_WORDLEFTEND)
+                        self.SendScintilla(QSci.SCI_WORDLEFTEND)
                 elif e.key() == Qt.Key_U: # redo
                     self.setReadOnly(0)
                     for _ in range(self.getNumberPrefix()):
@@ -315,10 +369,10 @@ class NemeTextWidget(QsciScintilla):
             elif modifiers == Qt.ControlModifier:# CONTROL
                 if e.key() == Qt.Key_I: # page up
                     for _ in range(self.getNumberPrefix(True)):
-                        self.SendScintilla(QsciScintilla.SCI_PAGEUP)
+                        self.SendScintilla(QSci.SCI_PAGEUP)
                 elif e.key() == Qt.Key_K: # page down
                     for _ in range(self.getNumberPrefix(True)):
-                        self.SendScintilla(QsciScintilla.SCI_PAGEDOWN)
+                        self.SendScintilla(QSci.SCI_PAGEDOWN)
 
         # ==============================================================
         # Command Mode
@@ -348,11 +402,11 @@ class NemeTextWidget(QsciScintilla):
             return
 
         if newmode == EditorMode.Typing:
-            self.SendScintilla(QsciScintilla.SCI_SETCARETSTYLE, 1)
+            self.SendScintilla(QSci.SCI_SETCARETSTYLE, 1)
             self.setReadOnly(0)
 
         elif newmode == EditorMode.Movement:
-            self.SendScintilla(QsciScintilla.SCI_SETCARETSTYLE, 2)
+            self.SendScintilla(QSci.SCI_SETCARETSTYLE, 2)
             self.setReadOnly(0)
             self.setReadOnly(1)
 
