b23a13de4bce0fdec07a0ec38d80ae5b12acca48
Juanjo Alvarez <juanjux@gmail.com>
2017-04-22 19:11:07 +0200

    Fixed some bugs, added static type checks, more tests

--- source/util/proxify.d
+++ source/util/proxify.d
@@ -1,14 +1,22 @@
 module neme.util.proxify;
 
-import std.conv: to;
-import std.traits;
+/**
+ * Authors: Juanjo Alvarez, juanjo@juanjoalvarez.net
+ *
+ * License: Boost License 1.0
+ *
+ * Bugs:
+ *  - Doesn't check if all the targets for a selector exist (but it shouldn't compile).
+ *  - Doesn't check if all the target methods for a given selector have the same signature.
+ *  - Doesn't copy the default parameter values from the targets.
+ */
 
 /**
- * This function will return as string that can be mixed into a Struct or Class to
- * generate delegates to any preexisting methods in the class with a name starting
- * with one of the specified Selectors and a "rebindProxies!selector" method that
- * can be used to switch from one set of alternative methods to others starting
- * with another name.
+ * This module provides a function that will return a string that can then be mixed
+ * into a Struct or Class to generate delegates to any preexisting methods in
+ * the class with a name starting with one of the specified Selectors and a
+ * "rebindProxies!selector" method that can be used to switch from one set of
+ * alternative methods to others starting with another name.
  *
  * This can be useful for example for having alternative implementations of some
  * methods and you want to be able to switch from one set to the other at runtime
@@ -49,11 +57,27 @@ import std.traits;
 
 // TODO: use UDA decorated functions instead of prefixes.
 // TODO: allow for optional rebindProxies name so the mixin can be used more
-//       than once for different sets of switchable methods.
+//       than once for different sets of switchable methods (and can follow
+//       the user naming conventions)
 // TODO: investigate how to recover the default parameters of the target method
 
-string Proxify(S, Selectors...)()
+import std.conv: to;
+
+string funcAttrsString(alias f)()
 {
+    string funcAttrs;
+    foreach (attr; __traits(getFunctionAttributes, f))
+    {
+        funcAttrs ~= to!string(attr) ~ " ";
+    }
+    return funcAttrs;
+}
+
+string Proxify(S=typeof(this), Selectors...)()
+    if(is(S == struct) || is(S == class))
+{
+    import std.traits: isSomeString;
+
     // delegateDecls will hold the code string for delegate declarations, like:
     // ReturnType!slow_bar delegate(Parameters!slow_bar) bar;
     // ReturnType!slow_bar delegate(Parameters!slow_bar) bar;
@@ -69,20 +93,26 @@ string Proxify(S, Selectors...)()
 
     foreach(selector; Selectors) // e.g. "slow_"
     {
-        selectorCases[selector] = "\tcase \"" ~ selector ~ "\":\n"; // case "slow_":
+        static assert(isSomeString!(typeof(selector)));
+        selectorCases[selector] = "case \"" ~ selector ~ "\": "; // case "slow_":
         string caseText;
 
         foreach(member; __traits(allMembers, S))
         {
+            // FIXME: check that member is a function
             static if (member.length > selector.length && member[0..selector.length] == selector) {
                 // if the selctor is "slow_" we've found a "slow_something"
 
                 static if (selector == Selectors[0]) {
                     // generate the delegate declaration copying the target function attributes
-                    enum attributes = to!string(__traits(getFunctionAttributes, __traits(getMember, S, member)));
+                    //string funcAttrs;
+                    //foreach (attr; __traits(getFunctionAttributes, __traits(getMember, S, member))) {
+                        //funcAttrs ~= attr ~ " ";
+                    //}
+                    enum attributes = funcAttrsString!(__traits(getMember, S, member));
                     enum declaration = to!string(attributes ~ " ReturnType!" ~ member ~
                                                   " delegate(Parameters!" ~ member ~ ") " ~
-                                                  member[5..$] ~ ";");
+                                                  member[selector.length..$] ~ ";");
                     delegateDecls ~= declaration;
                 }
 
@@ -99,9 +129,10 @@ string Proxify(S, Selectors...)()
         cases ~= case_ ~ "break;";
     }
     cases ~= "default: return;";
-    methodsSelector = "void rebindProxies(string S)() { switch(S){" ~ cases ~ "}}";
+    methodsSelector = "pure @safe @nogc void rebindProxies(string S)() { switch(S){" ~ cases ~ "}}";
 
-    return to!string(delegateDecls ~ "\n" ~ methodsSelector);
+    string imports = "import std.traits;\n";
+    return imports ~ to!string(delegateDecls ~ "\n" ~ methodsSelector);
 }
 
 ///
@@ -109,12 +140,13 @@ unittest
 {
     struct TestStruct
     {
-        mixin(Proxify!(typeof(this), "slow_", "fast_"));
-        string slow_foo(int param1) { return "slow foo"; }
-        string fast_foo(int param)  { return "fast foo"; }
+        @nogc @safe string slow_foo(int param1) { return "slow foo"; }
+        @nogc @safe string fast_foo(int param)  { return "fast foo"; }
 
-        string slow_bar(string param="default", int param2=4) { return "slow bar"; }
-        string fast_bar(string param="default", int param2=4) { return "fast bar"; }
+        pure string slow_bar(string param="default", int param2=4) { return "slow bar"; }
+        pure string fast_bar(string param="default", int param2=4) { return "fast bar"; }
+
+        mixin(Proxify!(TestStruct, "slow_", "fast_"));
     }
 
     TestStruct m;
@@ -132,17 +164,27 @@ unittest
     assert(m.foo(3) == m.slow_foo(3));
     assert(m.bar == &m.slow_bar);
     assert(m.foo == &m.slow_foo);
+
+    import std.traits;
+    assert(__traits(getFunctionAttributes, m.foo) ==
+           __traits(getFunctionAttributes, m.slow_foo));
+    assert(__traits(getFunctionAttributes, m.foo) ==
+           __traits(getFunctionAttributes, m.fast_foo));
+
+    assert(__traits(getFunctionAttributes, m.bar) ==
+           __traits(getFunctionAttributes, m.slow_bar));
+    assert(__traits(getFunctionAttributes, m.bar) ==
+           __traits(getFunctionAttributes, m.fast_bar));
 }
 
-
-/* Example generated code:
+/* Example generated code (prettyfied):
 
 Proxify!(TestStruct, "fast_", "slow_");
 
 @system ReturnType!slow_foo delegate(Parameters!slow_foo) foo;
 @system ReturnType!slow_bar delegate(Parameters!slow_bar) bar;
 
-void rebindProxies(string S)() {
+pure @nogc @safe void rebindProxies(string S)() {
     switch(S)
     {
         case "fast_":
